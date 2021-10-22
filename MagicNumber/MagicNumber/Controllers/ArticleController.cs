1d7a4f06403d36250ce31a1cd91a18528d180249
Dieu Le <18520265@gm.uit.edu.vn>
2021-10-22 17:41:52 +0700

    Module Read Article: Update CRUD API for both ArticleTypeController& ArticleController

--- MagicNumber/MagicNumber/Controllers/ArticleController.cs
+++ MagicNumber/MagicNumber/Controllers/ArticleController.cs
@@ -11,8 +11,35 @@ namespace MagicNumber.Controllers
 {
     public class ArticleController : ApiController
     {
+		[System.Web.Http.Route("api/Article/GetAll")]
+		[System.Web.Http.HttpGet]
+		public HttpResponseMessage GetAll()
+		{
+			string sql = $" SELECT * FROM article  ";
+			MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+			MySqlCommand cmd = new MySqlCommand(sql, con);
+			con.Open();
+
+			MySqlDataReader reader = cmd.ExecuteReader();
+			
+			List<Article> temp = new List<Article>();
+			while (reader.Read())
+			{
+				Article temp1 = new Article();
+				temp1.ArticleID = reader.GetString("ArticleID");
+				temp1.Detail = reader.GetString("Detail");
+				temp1.Title = reader.GetString("Title");
+				temp1.Upvote = reader.GetString("Upvote");
+				temp1.ArticleTypeID = reader.GetString("ArticleTypeID");
+				temp.Add(temp1);
+			}
+			con.Close();
+			return Request.CreateResponse(System.Net.HttpStatusCode.OK, temp);
+		}
 		// GET: Article
-		public HttpResponseMessage Get(string id)
+		[System.Web.Http.Route("api/Article/GetByID")]
+		[System.Web.Http.HttpGet]
+		public HttpResponseMessage GetByID(string id)
 		{
 			string sql = $" SELECT * FROM article where articleID='{id}'  ";
 			MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
@@ -27,11 +54,39 @@ namespace MagicNumber.Controllers
 				temp.Detail= reader.GetString("Detail");
 				temp.Title= reader.GetString("Title");
 				temp.Upvote= reader.GetString("Upvote");
+				temp.ArticleTypeID = reader.GetString("ArticleTypeID");
 			}
 			con.Close();
 			return Request.CreateResponse(System.Net.HttpStatusCode.OK, temp);
 		}
-		public string Post(Article article)
+
+		[System.Web.Http.Route("api/Article/GetByType")]
+		[System.Web.Http.HttpGet]
+		public HttpResponseMessage GetByType(string typeId)
+		{
+			string sql = $" SELECT * FROM article where articleTypeID='{typeId}'  ";
+			MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+			MySqlCommand cmd = new MySqlCommand(sql, con);
+			con.Open();
+
+			MySqlDataReader reader = cmd.ExecuteReader();
+			List<Article> temp = new List<Article>();
+			while (reader.Read())
+			{
+				Article temp1 = new Article();
+				temp1.ArticleID = reader.GetString("ArticleID");
+				temp1.Detail = reader.GetString("Detail");
+				temp1.Title = reader.GetString("Title");
+				temp1.Upvote = reader.GetString("Upvote");
+				temp1.ArticleTypeID = reader.GetString("ArticleTypeID");
+				temp.Add(temp1);
+			}
+			con.Close();
+			return Request.CreateResponse(System.Net.HttpStatusCode.OK, temp);
+		}
+		[System.Web.Http.Route("api/Article/Add")]
+		[System.Web.Http.HttpPost]
+		public string Add(Article article)
 		{
 			try
 			{
@@ -45,6 +100,27 @@ namespace MagicNumber.Controllers
 			catch (Exception)
 			{
 				return "Fail to add";
+			}
+		}
+
+		[System.Web.Http.Route("api/Article/Edit")]
+		[System.Web.Http.HttpPut]
+		public string Edit(Article article)
+		{
+			try
+			{
+				string sql = $" update article set Title='{article.Title}', Detail='{article.Detail}', Upvote='{article.Upvote}',ArticleTypeID= '{article.ArticleTypeID}'  where ArticleID='{article.ArticleID}'";
+				MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+				MySqlCommand cmd = new MySqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return "Edit successfully!";
+			}
+			catch (Exception e)
+			{
+				
+				return $"Fail to edit {e.Message}";
+
 			}
 		}
 	}
---
+++ MagicNumber/MagicNumber/Controllers/TypeArticleController.cs
@@ -0,0 +1,113 @@
+﻿using MagicNumber.Models;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MagicNumber.Controllers
+{
+    public class TypeArticleController : ApiController
+	{
+		// GET: Article
+		[System.Web.Http.Route("api/ArticleType/GetByID")]
+		[System.Web.Http.HttpGet]
+		public HttpResponseMessage GetByID(string id)
+		{
+			string sql = $" SELECT * FROM articleType where articleTypeID='{id}'  ";
+			MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+			MySqlCommand cmd = new MySqlCommand(sql, con);
+			con.Open();
+
+			MySqlDataReader reader = cmd.ExecuteReader();
+			ArticleType temp = new ArticleType();
+			while (reader.Read())
+			{
+				temp.ArticleTypeID = reader.GetString("ArticleTypeID");
+				temp.Detail = reader.GetString("Detail");
+				temp.Name = reader.GetString("Name");
+			}
+			con.Close();
+			return Request.CreateResponse(System.Net.HttpStatusCode.OK, temp);
+		}
+		[System.Web.Http.Route("api/ArticleType/GetAll")]
+		[System.Web.Http.HttpGet]
+		public HttpResponseMessage GetAll()
+		{
+			string sql = $" SELECT * FROM articleType   ";
+			MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+			MySqlCommand cmd = new MySqlCommand(sql, con);
+			con.Open();
+
+			MySqlDataReader reader = cmd.ExecuteReader();
+			List<ArticleType> temp = new List<ArticleType>();
+			while (reader.Read())
+			{
+				ArticleType temp1 = new ArticleType();
+				temp1.ArticleTypeID = reader.GetString("ArticleTypeID");
+				temp1.Detail = reader.GetString("Detail");
+				temp1.Name = reader.GetString("Name");
+				temp.Add(temp1);
+			}
+			con.Close();
+			return Request.CreateResponse(System.Net.HttpStatusCode.OK, temp);
+		}
+		[System.Web.Http.Route("api/ArticleType/Post")]
+		[System.Web.Http.HttpPost]
+		public string Post(ArticleType type)
+		{
+			try
+			{
+				string sql = $" Insert into articleType values('{type.ArticleTypeID}','{type.Name}','{type.Detail}')  ";
+				MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+				MySqlCommand cmd = new MySqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return "Add successfully!";
+			}
+			catch (Exception)
+			{
+				return "Fail to add";
+			}
+		}
+		[System.Web.Http.Route("api/ArticleType/Put")]
+		[System.Web.Http.HttpPut]
+		public string Put(ArticleType type)
+		{
+			try
+			{
+				string sql = $" update articleType set Name='{type.Name}', Detail='{type.Detail}' where ArticleTypeID='{type.ArticleTypeID}'";
+				MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+				MySqlCommand cmd = new MySqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return "Edit successfully!";
+			}
+			catch (Exception)
+			{
+				return "Fail to edit";
+			}
+		}
+		[System.Web.Http.Route("api/ArticleType/Delete")]
+		[System.Web.Http.HttpDelete]
+		public string Delete(string id)
+		{
+			try
+			{
+				string sql = $" delete from ArticleType where ArticleTypeID='{id}'";
+				MySqlConnection con = new MySqlConnection("host=localhost;user=root;password='';database=numberum;");
+				MySqlCommand cmd = new MySqlCommand(sql, con);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				return $"Delete {id} successfully!";
+			}
+			catch (Exception)
+			{
+				return "Fail ";
+			}
+		}
+	}
+}
