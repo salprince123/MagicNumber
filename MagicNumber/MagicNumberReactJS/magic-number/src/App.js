9b89d80aa13d6b8ca7d6ba22e52d960a37a465f4
Dieu Le <18520265@gm.uit.edu.vn>
2021-10-28 19:19:03 +0700

    Update project template

--- MagicNumber/MagicNumberReactJS/magic-number/src/App.js
+++ MagicNumber/MagicNumberReactJS/magic-number/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+/*import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Number from './API/Number';
 import React from 'react';
@@ -40,6 +40,34 @@ function App() {
      </Switch>
     </div>
     </BrowserRouter>   
+  );
+}
+
+export default App;*/
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+
+import AppHeader from './components/common/header';
+import AppFooter from './components/common/footer';
+import AppHome from './views/home';
+
+import { Layout } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+function App() {
+  return (
+    <Layout className="mainLayout">
+      <Header>
+        <AppHeader/>
+      </Header>
+      <Content>
+        <AppHome/>
+      </Content>
+      <Footer>
+        <AppFooter/>  
+      </Footer>      
+    </Layout>
   );
 }
 
