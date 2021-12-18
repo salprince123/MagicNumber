bdb7119ba96e73c13aafbf49b9151610e09bb84a
Ha Phuong <59615434+TTph18@users.noreply.github.com>
2021-12-18 19:02:49 +0700

    Update pyramid

--- MagicNumber/MagicNumberReactJS/magic-number/src/views/number/details/index.js
+++ MagicNumber/MagicNumberReactJS/magic-number/src/views/number/details/index.js
@@ -24,69 +24,87 @@ import { Fragment, useState, useEffect } from 'react'
 import axios from 'axios'
 
 const NumberForm = () => {
-    const temp= window.location.href.replace("http://localhost:3000/find-number/result/","").replace("%7D","").replace("&","\\").replaceAll("%20"," ")
+    const temp = window.location.href.replace("http://localhost:3000/find-number/result/", "").replace("%7D", "").replace("&", "\\").replaceAll("%20", " ")
     const array = temp.split("\\")
-    const url="http://localhost:7999/api/Number/SubmitForm"
-    const mainNumber={
+    const url = "http://localhost:7999/api/Number/SubmitForm"
+    const mainNumber = {
         "NumberID": "",
         "Title": "",
-        "Detail":"ahahah" 
+        "Detail": "ahahah"
     }
-    const tempData={
-        "returnNumber":mainNumber,
+    const tempData = {
+        "returnNumber": mainNumber,
         "birthdayChart": []
-        }
-    const [name, setName]= useState(array[1])
-    const [birthday, setBirthday]= useState(array[0])
-    const [data, setData]= useState(null)   
-    useEffect(() => {     
+    }
+    const [name, setName] = useState(array[1])
+    const [birthday, setBirthday] = useState(array[0])
+    const [data, setData] = useState(null)
+    useEffect(() => {
         axios.get(url, {
             params: {
                 date: birthday
             }
         }).then(res => setData(res.data[0]));
-      }, [])
+    }, [])
 
     return (
         <div>
-        {data !== null ? (        
-        <div>
-        <Card>            
-            <CardHeader>
-                <CardTitle tag='h4'>Bạn sinh ngày {birthday[0]}{birthday[1]}/{birthday[2]}{birthday[3]}/{birthday[4]}{birthday[5]}{birthday[6]}{birthday[7]}</CardTitle>
-                </CardHeader>
-            <CardHeader>
-                <CardTitle tag='h4'>Con số chủ đạo(con số đường đời) của bạn là {data.returnNumber.NumberID[2]}{data.returnNumber.NumberID[3]}</CardTitle>
-            </CardHeader>
-            <CardBody>
-                <Label >{data.returnNumber.Detail}</Label>
-               
-            </CardBody>
-        </Card>
-        <Card>            
-            <CardHeader>
-                <CardTitle tag='h4'>Sau đây là biểu đồ ngày sinh của bạn</CardTitle>
-            </CardHeader>
-            <CardBody>                
-                <div class="board">
-                    <button disabled={true} class="squares">{data.birthdayChart[3]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[6]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[9]}</button>
+            {data !== null ? (
+                <div>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle tag='h4'>Bạn sinh ngày {birthday[0]}{birthday[1]}/{birthday[2]}{birthday[3]}/{birthday[4]}{birthday[5]}{birthday[6]}{birthday[7]}</CardTitle>
+                        </CardHeader>
+                        <CardHeader>
+                            <CardTitle tag='h4'>Con số chủ đạo(con số đường đời) của bạn là {data.returnNumber.NumberID[2]}{data.returnNumber.NumberID[3]}</CardTitle>
+                        </CardHeader>
+                        <CardBody>
+                            <Label >{data.returnNumber.Detail}</Label>
 
-                    <button disabled={true} class="squares">{data.birthdayChart[2]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[5]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[8]}</button>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle tag='h4'>Sau đây là biểu đồ ngày sinh của bạn</CardTitle>
+                        </CardHeader>
+                        <CardBody>
+                            <div class="board">
+                                <button disabled={true} class="squares">{data.birthdayChart[3]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[6]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[9]}</button>
 
-                    <button disabled={true} class="squares">{data.birthdayChart[1]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[4]}</button>
-                    <button disabled={true} class="squares">{data.birthdayChart[7]}</button>
-                    
+                                <button disabled={true} class="squares">{data.birthdayChart[2]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[5]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[8]}</button>
+
+                                <button disabled={true} class="squares">{data.birthdayChart[1]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[4]}</button>
+                                <button disabled={true} class="squares">{data.birthdayChart[7]}</button>
+
+                            </div>
+                            <Col sm='12'>
+                                <ChartTree 
+                                dateNumber={} 
+                                monthNumber={} 
+                                yearNumber={}/>
+                            </Col>
+                            <Col sm='12'>
+                                <FormGroup className='d-flex mb-0' >
+                                    <Label tag="h4">Tháng 5</Label>
+                                    <div className="px-5" />
+                                    <div className="px-5" />
+                                    <div className="px-3" />
+                                    <Label className="px-5" tag="h4">Ngày 1</Label>
+                                    <div className="px-5" />
+                                    <div className="px-5" />
+                                    <div className="px-3" />
+                                    <Label className='px-1' tag="h4">Năm 1974</Label>
+                                </FormGroup>
+                            </Col>
+                        </CardBody>
+                    </Card>
                 </div>
-                
-            </CardBody>
-        </Card>
-        </div>
-        ) : null}
+            ) : null}
         </div>
     )
 }
