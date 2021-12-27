2ad8b684ee7274efde05fbf5b27563e0abc463e2
Ha Phuong <59615434+TTph18@users.noreply.github.com>
2021-12-27 14:59:06 +0700

    Add package and controller

---
+++ MagicNumber/MagicNumber/Controllers/AuthenController.cs
@@ -0,0 +1,70 @@
+﻿using MagicNumber.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MagicNumber.Request;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MagicNumber.Controllers
+{
+    public class AuthenController : ApiController
+    {
+        private readonly UserManager<User> _userManager;
+        public AuthenController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> Login([FromBody] LoginRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(request.Username);
+                if (user == null)
+                {
+                    return BadRequest("Username or password is incorrect. Please try again");
+                }
+                var isCorrect = await _userManager.CheckPasswordAsync(user, request.Password);
+                if (!isCorrect)
+                {
+                    return BadRequest("Username or password is incorrect. Please try again");
+                }
+                if (user != null && await _userManager.CheckPasswordAsync(user, request.Password))
+                {
+                    var userRoles = await _userManager.GetRolesAsync(user);
+                    var authClaims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, request.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    };
+                    foreach (var userRole in userRoles)
+                    {
+                        authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                    }
+                    var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("C1CF4B4DC1C4177B7618DE4F55CA2"));
+                    var token = new JwtSecurityToken(
+                        issuer: "MagicNumber",
+                        audience: "MagicNumber.User",
+                        expires: DateTime.Now.AddHours(3),
+                        claims: authClaims,
+                        signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256)
+                        );
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        User = user.UserName,
+                        Role = userRoles,
+                    });
+                }
+            }
+            return Unauthorized();
+        }
+    }
+}
--- MagicNumber/MagicNumber/Models/User.cs
+++ MagicNumber/MagicNumber/Models/User.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MagicNumber.Models
 {
-    public class User
+    public class User : IdentityUser<int>
     {
-        public string UserID { get; set; }
-        public string Password { get; set; }
+        [PersonalData]
         public string Name { get; set; }
+        [PersonalData]
         public string About { get; set; }
+        [PersonalData]
         public string Quote { get; set; }
-
+        [PersonalData]
         public string Birthday { get; set; }
+        [PersonalData]
         public string Avatar { get; set; }
+        [PersonalData]
         public string Background { get; set; }
 
     }
